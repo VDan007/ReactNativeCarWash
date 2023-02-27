291e9a17240f844dcf01e93a6480e606a9753569
DanielVarjaskéri <varjaskerid@gmail.com>
2023-02-28 00:50:31 +0100

    footer added

--- components/FooterNav.js
+++ components/FooterNav.js
@@ -1,17 +1,32 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 function FooterNav(){
     return(
         <View style={styles.footer}>
-            <View>
+            <View style={styles.footerBtn}>
+                <Icon
+                name="home"
+                style={styles.icon}
+                ></Icon>
                 <Text>Home</Text>
             </View>
-            <View>
-            <Text>Map</Text>
+
+            <View style={styles.footerBtn}>
+                <Icon
+                name="map"
+                style={styles.icon}
+                ></Icon>
+                <Text>Map</Text>
             </View>
-            <View>
-            <Text>Account</Text>
+
+            <View style={styles.footerBtn}>
+                <Icon
+                name="user"
+                style={styles.icon}
+                ></Icon>
+                <Text>Account</Text>
             </View>
 
         </View>
@@ -23,12 +38,20 @@ const styles = StyleSheet.create({
         position: "absolute",
         flexDirection: "row",
         justifyContent: "space-between",
-        paddingHorizontal: 15,
+        paddingHorizontal: 25,
         bottom:0,
-        height: 80,
+        height: 85,
         width: "100%",
         backgroundColor: "#B5EB4A"
-    }
+    },
+    icon:{
+        fontSize: 35,
+    },
+    footerBtn:{
+        justifyContent:"center",
+        alignItems: 'center',
+        gap: 3,
+    },
 });
 
 export default FooterNav;
